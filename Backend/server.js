8052b1aa2684838bced06f4bd050cf8dfbbf08d7
Developer-Gopal <gopalkrishnar392@gmail.com>
2025-09-22 20:23:46 +0530

    Update server.js with new .env

--- Backend/server.js
+++ Backend/server.js
@@ -25,9 +25,8 @@ admin.initializeApp({
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   }),
   databaseURL: process.env.FIREBASE_DB_URL,
 });
